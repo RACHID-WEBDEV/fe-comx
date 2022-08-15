c0a65c20e281021b32084ac9ddd63ee5e62c77ef
github.com/RACHID-WEBDEV <rasheedolamide2015@gmail.com>
2022-08-15 18:22:39 +0100

    Modified Alert presentation

--- components/form/Alert.js
+++ components/form/Alert.js
@@ -12,23 +12,22 @@ const Alert = () => {
 
   return (
     <>
-      {currentErrors.length > 0 &&
-        currentErrors.map(({ index, message }) => (
-          <>
-            {showAlert && (
-              <div
-                key={index}
-                className="flex items-center justify-between p-3 pr-4 mb-2 border rounded-[10px] border-red-200 bg-red-100"
-              >
-                <strong className="text-sm pl-1 font-Roboto font-semibold text-red-200">{message} </strong>
-                <button className="opacity-100" type="button" onClick={() => setShowAlert(false)}>
-                  <span className="sr-only"> Close </span>
-                  <XIcon width="11px" height="11px" fill="#EF4444" />
-                </button>
-              </div>
-            )}
-          </>
-        ))}
+      {showAlert && currentErrors.length > 0 && (
+        <div className="flex items-center justify-between p-3 pr-4 mb-2 border rounded-[10px] border-red-200 bg-red-100">
+          <ul>
+            {currentErrors.map(({ index, message }) => (
+              <li key={index} className="text-left my-1">
+                <strong className="text-sm pl-1 font-Roboto font-semibold text-red-200">{message}</strong>
+              </li>
+            ))}
+          </ul>
+
+          <button className="opacity-100" type="button" onClick={() => setShowAlert(false)}>
+            <span className="sr-only"> Close </span>
+            <XIcon width="11px" height="11px" fill="#EF4444" />
+          </button>
+        </div>
+      )}
     </>
   );
 };
